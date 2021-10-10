369af9a7dae58fb4fd6ad77a0cd238f8d6554c8c
LiamJansen <62886309+LiamJansen@users.noreply.github.com>
2021-10-11 01:17:11 +0200

    uncommented some stuff for better readability in peer-review

--- loops.js
+++ loops.js
@@ -6,10 +6,9 @@
 // Hoera!
 // ==========================================
 
-// for (let i = 0; i < 3; i++) {
-        // console.log("Hoera!");
-    // }
-
+for (let i = 0; i < 3; i++) {
+        console.log("Hoera!");
+    }
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -21,15 +20,14 @@
 // klaar!
  // ==========================================
 
-//  const final_number = 4;
+ const final_number = 4;
 
-//  for (let i = 0; i < final_number; i++) {
-//         console.log("loop...");
-//         if (i === (final_number - 1)) {
-//             console.log("klaar!");
-//         }
-//     }
-
+ for (let i = 0; i < final_number; i++) {
+        console.log("loop...");
+        if (i === (final_number - 1)) {
+            console.log("klaar!");
+        }
+    }
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -44,13 +42,13 @@
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 
-// for (let i = 0; i < 16; i++) {
-//     if (i % 2) {
-//         console.log(i + " is oneven");
-//     } else {
-//         console.log (i + " is even");
-//     }
-// }
+for (let i = 0; i < 16; i++) {
+    if (i % 2) {
+        console.log(i + " is oneven");
+    } else {
+        console.log (i + " is even");
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -68,14 +66,14 @@
 // >> 9
 // ==========================================
 
-// for (let i = 0; i < 10; i++) {
-//     if (i < 3) {
-//         console.log(i);
-//     }
-//     else if (i > 2 && i  < 7) {
-//         console.log("> " + i);
-//     }
-//     else if (i > 6) {
-//         console.log(">> " + i);
-//     }
-// }
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
+    }
+    else if (i > 2 && i  < 7) {
+        console.log("> " + i);
+    }
+    else if (i > 6) {
+        console.log(">> " + i);
+    }
+}
